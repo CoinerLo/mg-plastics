192b3bd3483b043fe9c06a5664a97e483aa6056b
ATMR90 <Art1603@yandex.ru>
2024-02-13 12:34:04 +0300

    Добавлен слайдер ко всем разделам

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import DemoSlider from '@/components/Slider/DemoSlider'
-import Slider from '@/components/Slider/Slider'
-import { imgParams } from '@/consts/imgParams'
 import { Box, Typography, FormGroup } from '@mui/material'
 
 import dataSlider from "@/consts/slider-data.json";
 
 const Home = () => {
+  const dataSliderMT = dataSlider.filter(thisEl => [1,2,3,21].includes(thisEl.id))
+  const dataSliderEM = dataSlider.filter(thisEl => [5,6,7,10,16,18,20,22].includes(thisEl.id))
+  const dataSliderII = dataSlider.filter(thisEl => [17,24].includes(thisEl.id))
+  const dataSliderQS = dataSlider.filter(thisEl => [4,9,11,12,13,14,15,19,23,25].includes(thisEl.id))
   return (
     <Box>
       <Box sx={{ height: '100vh', pt: 14 }}>
@@ -18,23 +20,25 @@ const Home = () => {
         <Typography variant='body1' sx={{ mt: 14 }}>
           Modernste Technologie: Wir investieren in die neuesten CNC-Dreh- und Fräsmaschinen, um präzise und effiziente Bearbeitung zu gewährleisten. Unsere Anlagen sind auf dem neuesten Stand der Technik und bereit, den Anforderungen moderner Produkte gerecht zu werden.
         </Typography>
-        <DemoSlider data={dataSlider} />
-        {/* <Slider images={['/img/Modernste-Technologie/1.jpg','/img/Modernste-Technologie/2.jpg','/img/Modernste-Technologie/3.jpg','/img/Modernste-Technologie/21.jpg']} imgParams={imgParams} heightDefault={500} key={'slider-1'} /> */}
+        <DemoSlider data={dataSliderMT} />
       </Box>
       <Box sx={{ height: '100vh', pt: 14 }} id='Engagierte Montage'>
         <Typography variant='body1' sx={{ mt: 14 }}>
           Engagierte Montage: Trotz unserer kurzen Geschichte legen wir großen Wert auf die Qualität der Montage. Jedes Teil wird sorgfältig geprüft und montiert, um höchste Leistung und Zuverlässigkeit zu gewährleisten.
         </Typography>
+        <DemoSlider data={dataSliderEM} />
       </Box>
       <Box sx={{ height: '100vh', pt: 14 }} id='Qualifiziertes Schweißen'>
         <Typography variant='body1' sx={{ mt: 14 }}>
           Qualifiziertes Schweißen: Wir verstehen die Bedeutung einer starken und haltbaren Verbindung in Kunststoffprodukten. Unsere Schweißexperten arbeiten mit den neuesten Verfahren, um sicherzustellen, dass jedes Produkt lange hält.
         </Typography>
+        <DemoSlider data={dataSliderQS} />
       </Box>
       <Box sx={{ height: '100vh', pt: 14 }} id='Innovative Ingenieurarbeit'>
         <Typography variant='body1' sx={{ mt: 14 }}>
           Innovative Ingenieurarbeit: Unser Team besteht aus leidenschaftlichen Ingenieuren, die ständig nach innovativen Wegen suchen, um die Produktivität zu steigern und die Kosten zu senken. Wir sind hier, um mit Ihnen zusammenzuarbeiten und maßgeschneiderte Lösungen zu entwickeln, die Ihren spezifischen Bedürfnissen entsprechen.
         </Typography>
+        <DemoSlider data={dataSliderII} />
       </Box>
       <Box sx={{ height: '100vh', pt: 14 }}>
         <Typography variant='h3'>Unser Engagement:</Typography>
