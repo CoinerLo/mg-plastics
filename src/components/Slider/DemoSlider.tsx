4040796dce7bd3100d9a4f4c0761248685668415
ATMR90 <Art1603@yandex.ru>
2024-02-26 09:43:07 +0300

    Убрал фон и пагинацию в слайдере, исправил стиль для мобилки

--- src/components/Slider/DemoSlider.tsx
+++ src/components/Slider/DemoSlider.tsx
@@ -31,7 +31,7 @@ const DemoSlider: React.FC<DemoSliderProps> = ({ data }) => {
         <ul style={{height: '100%', width: '100%'}}>
           <Swiper
             navigation
-            pagination={{ type: "bullets", clickable: true }}
+            // pagination={{ type: "bullets", clickable: true }}
             // autoplay={true}
             loop={true}
             modules={[Autoplay, Navigation, Pagination]}
@@ -47,8 +47,8 @@ const DemoSlider: React.FC<DemoSliderProps> = ({ data }) => {
               <SwiperSlide key={id}>
                 {/* <Box sx={{height: '100%', width: '100%', position: 'absolute', left: '0px', top: '0px', background: `url(${image}) center center / contain scroll no-repeat`}}>
                 </Box> */}
-                <Box sx={{height: '100%', width: '100%', position: 'absolute', left: '0', top: '0', opacity: '0.2', backgroundColor: 'rgb(0 0 0 / 1)'}}>
-                </Box>
+                {/* <Box sx={{height: '100%', width: '100%', position: 'absolute', left: '0', top: '0', opacity: '0', backgroundColor: 'rgb(0 0 0 / 1)'}}>
+                </Box> */}
                 <Box sx={{height: '100%', position: 'relative', zIndex: '10', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                   <Box sx={{textAlign: 'center', height: '500px'}}>
                     <Image src={image} alt={alt}
